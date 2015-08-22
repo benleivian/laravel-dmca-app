12d3dde2adabb2eb7d2fa93ae3f413020c344b8f
Ben Leivian <ben@drawbackwards.com>
2015-08-21 21:14:29 -0700

    Get a list from the provider table and send to view (select element).

--- app/Http/Controllers/NoticesController.php
+++ app/Http/Controllers/NoticesController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
+use \App\Provider;
+
 class NoticesController extends Controller
 {
 
@@ -22,10 +24,17 @@ class NoticesController extends Controller
 
     public function create()
     {
-    	// Get list of providers
+    	// Get list of providers.
+        $providers = Provider::lists('name', 'id');
 
-    	// Load a view to create a new notice
-    	return view('notices.create');
+    	// Load a view to create a new notice.
+    	return view('notices.create', compact('providers'));
+    }
+
+    public function confirm(Requests\PrepareNoticeRequest $request)
+    {
+        // Review the form data.
+        return $request->all();
     }
 
 }
