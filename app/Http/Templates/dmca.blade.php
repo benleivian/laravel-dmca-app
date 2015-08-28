ae25a1e05c6ef383ced8f75c31b10586b4145dd4
Ben Leivian <ben@drawbackwards.com>
2015-08-27 23:32:52 -0700

    Create quick dmca template.

---
+++ app/Http/Templates/dmca.blade.php
@@ -0,0 +1,15 @@
+Infringing Title:
+{{ $infringing_title }}
+
+Infringing Link
+{{ $infringing_link }}
+
+Original Link
+{{ $original_link }}
+
+Original Description
+{{ $original_description }}
+
+Sincerely,
+{{ $name }}
+{{ $email }}
