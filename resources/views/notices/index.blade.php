46edf0a9d567bcdde5a5d60301409db8490a816f
Ben Leivian <ben@drawbackwards.com>
2015-08-30 21:04:58 -0700

    Update view to list Notices (basic).

--- resources/views/notices/index.blade.php
+++ resources/views/notices/index.blade.php
@@ -4,19 +4,23 @@
 
 <div class="jumbotron">
     <div class="container">
-      <h1>Notices</h1>
+        <h1>Notices</h1>
     </div>
 </div>
 
 <div class="container markdown-body">
     <div class="columns">
-      <div class="column centered">
+        <div class="column centered">
 
-          <div class="btn-group" style="margin-top:2rem;">
-            <a class="btn" href="{{ url('/notices/create') }}" role="button">Create Notice</a>
-          </div>
+            @foreach ($notices as $notice)
+            <p>Notice: {{ $notice->id }}</p>
+            @endforeach
 
-      </div>
+            <div class="btn-group" style="margin-top:2rem;">
+                <a class="btn" href="{{ url('/notices/create') }}" role="button">Create Notice</a>
+            </div>
+
+        </div>
     </div>
 </div>
 
