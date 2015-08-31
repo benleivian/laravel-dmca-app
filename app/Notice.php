595f2d9a71d26730dcfee8f0d155a210d359828c
Ben Leivian <ben@drawbackwards.com>
2015-08-30 22:29:50 -0700

    Setup Notice relationship & helper methods.

--- app/Notice.php
+++ app/Notice.php
@@ -20,13 +20,52 @@ class Notice extends Model
 	/**
 	 * Open a new notice.
 	 *
-	 * @param  array $attributes
-	 * @return static
+	 * @param  array  $attributes
+	 * @return
 	 */
-    public static function open(array $attributes)
-    {
-    	// new Notice(array)
-    	return new static($attributes);
-    }
+	public static function open(array $attributes)
+	{
+		// new Notice(array)
+		return new static($attributes);
+	}
 
+	/**
+	 * A notice belongs to a single recipient/provider.
+	 *
+	 * @return
+	 */
+	public function provider()
+	{
+		return $this->belongsTo('App\Provider');
+	}
+
+	/**
+	 * A notice is created by a single user.
+	 *
+	 * @return
+	 */
+	public function user()
+	{
+		return $this->belongsTo('App\User');
+	}
+
+	/**
+	 * Get the email address for the recipient of the DMCA.
+	 *
+	 * @return string
+	 */
+	public function getRecipientEmail()
+	{
+		return $this->provider->copyright_email;
+	}
+
+	/**
+	 * Get the email address of the notice owner.
+	 *
+	 * @return string
+	 */
+	public function getOwnerEmail()
+	{
+		return $this->user->email;
+	}
 }
